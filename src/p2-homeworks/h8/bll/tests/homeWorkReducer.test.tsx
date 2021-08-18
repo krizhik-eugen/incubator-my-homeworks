25b1e2dda8297a6ea5d4c4bfa8df0e4891db338c
Eugen <krizhik@gmail.com>
2021-08-18 18:15:50 +0300

    HW8 updated

--- src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
+++ src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
@@ -1,37 +1,40 @@
 import React from 'react'
 import {homeWorkReducer} from '../homeWorkReducer'
-import {UserType} from "../../HW8";
+import {InitialStateType} from "../../HW8";
 
-let initialState: UserType[] // need to fix any
+let initialState: InitialStateType
 
 beforeEach(() => {
-    initialState = [
-        {_id: 0, name: 'Кот', age: 3},
-        {_id: 1, name: 'Александр', age: 66},
-        {_id: 2, name: 'Коля', age: 16},
-        {_id: 3, name: 'Виктор', age: 44},
-        {_id: 4, name: 'Дмитрий', age: 40},
-        {_id: 5, name: 'Ирина', age: 55},
-    ]
+    initialState = {
+        people: [
+            {_id: 0, name: 'Кот', age: 3},
+            {_id: 1, name: 'Александр', age: 66},
+            {_id: 2, name: 'Коля', age: 16},
+            {_id: 3, name: 'Виктор', age: 44},
+            {_id: 4, name: 'Дмитрий', age: 40},
+            {_id: 5, name: 'Ирина', age: 55}
+        ],
+        sorted: 'unsorted'
+    }
 })
 
 test('sort name up', () => {
-    const newState: UserType[] = homeWorkReducer(initialState, {type: 'sort', payload: 'up'})
+    const newState: InitialStateType = homeWorkReducer(initialState, {type: 'sort', payload: 'up'})
 
     console.log(newState)
-    expect(newState[0].name).toBe('Александр')
-    expect(newState[5].name).toBe('Кот')
+    expect(newState.people[0].name).toBe('Александр')
+    expect(newState.people[5].name).toBe('Кот')
 })
 test('sort name down', () => {
-    const newState: UserType[] = homeWorkReducer(initialState, {type: 'sort', payload: 'down'})
-    expect(newState[0].name).toBe('Кот')
-    expect(newState[5].name).toBe('Александр')
+    const newState: InitialStateType = homeWorkReducer(initialState, {type: 'sort', payload: 'down'})
+    expect(newState.people[0].name).toBe('Кот')
+    expect(newState.people[5].name).toBe('Александр')
 
 
 })
 test('check age 18', () => {
-    const newState: UserType[] = homeWorkReducer(initialState, {type: 'check', payload: 18})
-    const filteredState = newState.filter(p => (p.age < 18))
+    const newState: InitialStateType = homeWorkReducer(initialState, {type: 'check', payload: 18})
+    const filteredState = newState.people.filter(p => (p.age < 18))
     expect(filteredState.length).toBe(0)
 
 })
